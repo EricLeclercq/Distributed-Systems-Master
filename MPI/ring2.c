20758fadb155106576e6f1ebde53b95dfca040a4
EricLeclercq <leclercq@u-bourgogne.fr>
2022-09-28 17:52:24 +0200

    anneau avec boucle pour le nombre de tours (pas satisfaisant !)

---
+++ MPI/ring2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <mpi.h>
+#define MAX_TURN 9
+
+int main(int argc, char *argv[]) {
+  int num_tasks, my_rank, namelen;
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int message=1;
+  int n_turn=0;
+  int n=0;
+  MPI_Status status;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Get_processor_name(processor_name, &namelen);
+
+for (n_turn=0; n_turn < MAX_TURN; n_turn++)
+{
+  if (my_rank==0)
+  {
+    MPI_Send(&message, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    printf("La tâche %d a envoyé %d\n", my_rank, message);
+    MPI_Recv(&message, 1, MPI_INT, num_tasks - 1, 0, MPI_COMM_WORLD, &status);
+    printf("La tâche %d a reçu %d\n", my_rank, message);
+  }
+  else
+     {
+      MPI_Recv(&message, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD, &status);
+      printf("La tâche %d a reçu %d de %d\n", my_rank, message, my_rank - 1);
+      message++;
+      MPI_Send(&message, 1, MPI_INT, (my_rank + 1) % num_tasks, 0,MPI_COMM_WORLD);
+      printf("La tâche %d a envoyé %d à %d \n", my_rank, message, (my_rank + 1) % num_tasks);
+    }
+  }
+
+  MPI_Finalize();
+}
